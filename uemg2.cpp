706f46c059abb1363a67a2b4d48a1b1f527d6b4a
marcos035 <62987362+marcos035@users.noreply.github.com>
2020-08-04 15:18:14 -0300

    Add files via upload

---
+++ uemg2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	string nome;
+	
+	cout<<"qual seu nome :"<<endl;
+	cin>> nome;
+	
+	cout<<"?_? ja passou alcool em gel hj  ?  " <<nome<<endl;
+	
+	return 0; 
+}
